676acb131c1d97cf199b7a3d98f1b502a13ba9d3
Peter Barraud <barraud@adobe.com>
2020-08-28 09:58:13 +0530

    added timing

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-# which is more efficient
-# import math
-import timeit
+
 from math import sqrt, ceil
 
-class Primes:
-    def __init__(self, count):
-        self.__count = count
+# Only to time this method
+class TimePrime:
+    def __init__(self):
+        self.__count = 0
+
+    @property
+    def count(self):
+        return self.__count
+
+    @count.setter
+    def count(self, val):
+        self.__count = val
 
     def prime_list(self):
         # we're going to assume that 2 is a prime - without checking for it
@@ -17,8 +24,32 @@ class Primes:
                 pass
             else:
                 primes.append(x)
-        print(primes)
+
+def time_the_primes():
+    import timeit
+    timeprime = TimePrime()
+    for i in range(1, 11):
+        timeprime.count = i*10000
+        print("|{counter}|{time}s|".format(counter=timeprime.count, time=round(timeit.timeit(timeprime.prime_list, number=1),3)))
+
+
+def main():
+    count = 2000
+    # we're going to assume that 2 is a prime - without checking for it
+    primes = [2]
+    # now get a list of all the numbers greater than 1 and equal the square root of x (rounded up)
+    for x in range(2, count+1):
+        l = [x for x in range(2, ceil(sqrt(x)))] + primes
+        if not all([x%s for s in l]):
+            pass
+        else:
+            primes.append(x)
+    print(primes)
+
 
 if __name__ == "__main__":
-    primes = Primes(5000)
-    primes.prime_list()
+    # main()
+    time_the_primes()
+
+
+
--- readme.md
+++ readme.md
@@ -1,6 +1,24 @@
 # Primes
 I'm writing a Python scrip to find out all `prime numbers` from `1` to `n`
-If you chance upon this, please suggest changes to make it more Pythonic and (of course) faster
+
+**If you chance upon this, please suggest changes to make it more Pythonic and (of course) faster**
 
 So, this is how I've built it:
 
+For each number in the list, we're going to check if it's divisible by all previous primes.
+
+But only upto the square root of that number (_rounded up_)
+
+These are some timings (_on my Ubnutu_). Using **Python's** `timeit`:
+|Number of primes|Time|
+|:-: |:-:|
+|10000|0.291s|
+|20000|1.046s|
+|30000|2.219s|
+|40000|3.808s|
+|50000|5.893s|
+|60000|8.5s|
+|70000|11.797s|
+|80000|15.601s|
+|90000|19.657s|
+|100000|24.494s|
